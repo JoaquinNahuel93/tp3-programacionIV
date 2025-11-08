671fe59f2a74fa838409c2228d5807240e4dfd7e
Joaquin Gonzalez <joaquiinng@gmail.com>
2025-11-08 13:15:56 -0300

    Actualizacion de app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,16 @@ import { pool } from "./config/db.js";
 const app = express();
 app.use(express.json());
 
-// Ruta para listar alumnos
+app.get("/", async (req, res) => {
+  try {
+    const [result] = await pool.query("SELECT 1 + 1 AS resultado");
+    res.json({ conexion: "exitosa", resultado: result[0].resultado });
+  } catch (err) {
+    console.error("Error al conectar a la base:", err);
+    res.status(500).json({ error: "Error de conexión a la base de datos" });
+  }
+});
+//Prueba de conexión
 app.get("/alumnos", async (req, res) => {
   try {
     const [rows] = await pool.query("SELECT * FROM alumno");
@@ -14,8 +23,6 @@ app.get("/alumnos", async (req, res) => {
     res.status(500).json({ error: "Error al obtener alumnos" });
   }
 });
-
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+
