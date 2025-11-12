60715b056f6fd3b0bd1d0574332133873f2032b3
Joaquin Gonzalez <joaquiinng@gmail.com>
2025-11-12 16:35:30 -0300

    Se agregan cambios a app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,22 @@
 import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import passport from "passport";
 import { pool } from "./config/db.js";
+import { authConfig } from "./middlewares/auth.js";
+import authRouter from "./routes/auth.js";
+import alumnosRouter from "./routes/alumnos.js";
+import materiasRouter from "./routes/materias.js";
+import notasRouter from "./routes/notas.js";
+import { notFound, errorHandler } from "./middlewares/errors.js";
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
+authConfig();
+app.use(passport.initialize());
 
 app.get("/", async (req, res) => {
   try {
@@ -13,16 +27,17 @@ app.get("/", async (req, res) => {
     res.status(500).json({ error: "Error de conexión a la base de datos" });
   }
 });
-//Prueba de conexión
-app.get("/alumnos", async (req, res) => {
-  try {
-    const [rows] = await pool.query("SELECT * FROM alumno");
-    res.json(rows);
-  } catch (err) {
-    console.error("Error al obtener alumnos:", err);
-    res.status(500).json({ error: "Error al obtener alumnos" });
-  }
-});
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
 
+app.use("/auth", authRouter);
+app.use("/alumnos", alumnosRouter);
+app.use("/materias", materiasRouter);
+app.use("/notas", notasRouter);
+
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+);
+
+// Middlewares de error y 404
+app.use(notFound);
+app.use(errorHandler);
